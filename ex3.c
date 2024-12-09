80b46507cc74bcd02a3965e70d72c5ce19076377
leeanne <leeanne06r@gmail.com>
2024-12-09 17:22:09 +0200

    finished 3

--- ex3.c
+++ ex3.c
@@ -22,18 +22,104 @@ Assignment: ex3
 char brands[NUM_OF_BRANDS][BRANDS_NAMES] = {"Toyoga", "HyunNight", "Mazduh", "FolksVegan", "Key-Yuh"};
 char types[NUM_OF_TYPES][TYPES_NAMES] = {"SUV", "Sedan", "Coupe", "GT"};
 
-int cube[DAYS_IN_YEAR][NUM_OF_BRANDS][NUM_OF_TYPES];
-int days = 0;
-
-int brandIndex = 0;
-int firstTypeSum = 0;
-int secondTypeSum = 0;
-int thirdTypeSum = 0;
-int fourthTypeSum = 0;
-
-int processed[NUM_OF_BRANDS] = {0}; // Array to track which indices have been processed
+// Array to keep track of which indices have been processed. let them know why i use
+int processed[NUM_OF_BRANDS] = {0};
 int processedCount = 0;
 
+//prints the menu for the user
+void printMenu();
+
+//this function gets the car brand index and the four car type sums and inserts the data accordingly
+void enterBrandDailyData(int days, int cube[DAYS_IN_YEAR][NUM_OF_BRANDS][NUM_OF_TYPES], int brandIndex, int firstSum,
+                         int secondSum, int thirdSum, int fourthSum);
+
+//this function populates data for all car types
+void populateForAllBrands(int days, int cube[DAYS_IN_YEAR][NUM_OF_BRANDS][NUM_OF_TYPES], int brandIndex, int firstSum,
+                          int secondSum, int thirdSum, int fourthSum);
+
+void dailyStat(int day, int cube[DAYS_IN_YEAR][NUM_OF_BRANDS][NUM_OF_TYPES]);
+
+int main() {
+    int cube[DAYS_IN_YEAR][NUM_OF_BRANDS][NUM_OF_TYPES];
+    int days = 0;
+    int choice = 0;
+    int brandIndex = 0;
+    int firstTypeSum = 0;
+    int secondTypeSum = 0;
+    int thirdTypeSum = 0;
+    int fourthTypeSum = 0;
+
+    //initialize all of cube's values to -1
+    for (int i = 0; i < DAYS_IN_YEAR; i++) {
+        for (int j = 0; j < NUM_OF_BRANDS; j++) {
+            for (int k = 0; k < NUM_OF_TYPES; k++) {
+                cube[i][j][k] = -1;
+            }
+        }
+    }
+    printMenu();
+    scanf("%d", &choice);
+    while (choice != done) {
+        switch (choice) {
+            case addOne:
+                printf("What brand?\n");
+                scanf(" %d %d %d %d %d", &brandIndex, &firstTypeSum, &secondTypeSum, &thirdTypeSum, &fourthTypeSum);
+
+                if (brandIndex < 0 || brandIndex > NUM_OF_TYPES) {
+                    printf("This brand is not valid\n");
+                    break;
+                }
+
+                enterBrandDailyData(days, cube, brandIndex, firstTypeSum, secondTypeSum, thirdTypeSum, fourthTypeSum);
+
+                break;
+
+            case addAll:
+                //reset all data before inserting new
+                processedCount = 0;
+                for (int i = 0; i < NUM_OF_BRANDS; i++) {
+                    processed[i] = 0;
+                }
+
+                printf("No data for brands Toyoga HyunNight Mazduh FolksVegan Key-Yuh\nPlease complete the data\n");
+                scanf(" %d %d %d %d %d", &brandIndex, &firstTypeSum, &secondTypeSum, &thirdTypeSum, &fourthTypeSum);
+
+                if (brandIndex < 0 || brandIndex > NUM_OF_TYPES) {
+                    printf("This brand is not valid\n");
+                    break;
+                }
+
+                populateForAllBrands(days, cube, brandIndex, firstTypeSum, secondTypeSum, thirdTypeSum, fourthTypeSum);
+
+                while (processedCount < NUM_OF_BRANDS) {
+                    scanf(" %d %d %d %d %d", &brandIndex, &firstTypeSum, &secondTypeSum, &thirdTypeSum, &fourthTypeSum);
+                    populateForAllBrands(days, cube, brandIndex, firstTypeSum, secondTypeSum, thirdTypeSum,
+                                         fourthTypeSum);
+                }
+
+                break;
+
+            case stats:
+                int day = 0;
+                printf("What day would you like to analyze?\n");
+                scanf(" %d", &day);
+
+                while (cube[day][0][0] == -1 || day < 0 || day > DAYS_IN_YEAR) {
+                    printf("Please enter a valid day\nWhat day would you like to analyze?\n");
+                    scanf(" %d", &day);
+                }
+
+                dailyStat(day, cube);
+                break;
+            default:
+                printf("Invalid input\n");
+        }
+        printMenu();
+        scanf("%d", &choice);
+    }
+    printf("Goodbye!\n");
+    return 0;
+}
 
 void printMenu() {
     printf("Welcome to the Cars Data Cube! What would you like to do?\n"
@@ -46,8 +132,8 @@ void printMenu() {
         "7.exit\n");
 }
 
-//this function gets the car brand index and the four car type sums and inserts the data accordingly
-void enterBrandDailyData(int brandIndex, int firstSum, int secondSum, int thirdSum, int fourthSum) {
+void enterBrandDailyData(int days, int cube[DAYS_IN_YEAR][NUM_OF_BRANDS][NUM_OF_TYPES], int brandIndex, int firstSum,
+                         int secondSum, int thirdSum, int fourthSum) {
     days++;
     cube[days][brandIndex][0] = firstSum;
     cube[days][brandIndex][1] = secondSum;
@@ -55,14 +141,19 @@ void enterBrandDailyData(int brandIndex, int firstSum, int secondSum, int thirdS
     cube[days][brandIndex][3] = fourthSum;
 }
 
-//this function populates data for all car types
-void populateForAllBrands(int brandIndex, int firstSum, int secondSum, int thirdSum, int fourthSum) {
+void populateForAllBrands(int days, int cube[DAYS_IN_YEAR][NUM_OF_BRANDS][NUM_OF_TYPES], int brandIndex, int firstSum,
+                          int secondSum, int thirdSum, int fourthSum) {
     if (processed[brandIndex] == 0) {
         processed[brandIndex] = 1;
         processedCount++;
     }
 
-    enterBrandDailyData(brandIndex, firstSum, secondSum, thirdSum, fourthSum);
+    enterBrandDailyData(days, cube, brandIndex, firstSum, secondSum, thirdSum, fourthSum);
+
+    if (processedCount == NUM_OF_BRANDS) {
+        return;
+    }
+
     printf("No data for brands");
     // Only print the brands that haven't been processed
     for (int i = 0; i < NUM_OF_BRANDS; i++) {
@@ -74,82 +165,54 @@ void populateForAllBrands(int brandIndex, int firstSum, int secondSum, int third
     printf("\nPlease complete the data\n");
 }
 
-void dailyStat(int day) {
+void dailyStat(int day, int cube[DAYS_IN_YEAR][NUM_OF_BRANDS][NUM_OF_TYPES]) {
     int sum = 0;
-    int sumByBrand = 0;
-    int sumByType = 0;
+
+    int highestSumByBrand = 0;
+    int tempSumByBrand = 0;
+    int highestBrandIndex = 0;
+
+    int highestSumByType = 0;
+    int tempSumByType = 0;
+    int highestTypeIndex = 0;
+
     for (int i = 0; i < NUM_OF_BRANDS; i++) {
         for (int j = 0; j < NUM_OF_TYPES; j++) {
             sum += cube[day][i][j];
+            tempSumByBrand += cube[day][i][j];
         }
 
+        if (highestSumByBrand < tempSumByBrand) {
+            highestSumByBrand = tempSumByBrand;
+            highestBrandIndex = i;
+        }
+        tempSumByBrand = 0;
     }
+
+    for (int i = 0; i < NUM_OF_TYPES; i++) {
+        for (int j = 0; j < NUM_OF_BRANDS; j++) {
+            tempSumByType += cube[day][j][i];
+        }
+
+        if (highestSumByType < tempSumByType) {
+            highestSumByType = tempSumByType;
+            highestTypeIndex = i;
+        }
+
+        tempSumByType = 0;
+    }
+
+    printf(
+        "In day number %d:\nThe sales total was %d\nThe best sold brand with %d sales was %s\nThe best sold"
+        " type with %d sales was %s\n",
+        day, sum,
+        highestSumByBrand, brands[highestBrandIndex], highestSumByType, types[highestTypeIndex]);
 }
 
+void printBrandByIndex(int index) {
+    printf("%s", brands[index]);
+}
 
-int main() {
-    //initialize all of cube's values to -1
-    for (int i = 0; i < DAYS_IN_YEAR; i++) {
-        for (int j = 0; j < NUM_OF_BRANDS; j++) {
-            for (int k = 0; k < NUM_OF_TYPES; k++) {
-                cube[i][j][k] = -1;
-            }
-        }
-    }
-
-    int choice;
-    printMenu();
-    scanf("%d", &choice);
-    while (choice != done) {
-        switch (choice) {
-            case addOne:
-                printf("Please enter a car brand index and the daily sales sum for each car type: \n");
-                scanf(" %d %d %d %d %d", &brandIndex, &firstTypeSum, &secondTypeSum, &thirdTypeSum, &fourthTypeSum);
-
-                if (brandIndex < 0 || brandIndex > NUM_OF_TYPES) {
-                    printf("This brand is not valid\n");
-                    break;
-                }
-
-                enterBrandDailyData(brandIndex, firstTypeSum, secondTypeSum, thirdTypeSum, fourthTypeSum);
-
-                break;
-
-            case addAll:
-                printf("No data for brands Toyoga HyunNight Mazduh FolksVegan Key-Yuh\nPlease complete the data\n");
-                scanf(" %d %d %d %d %d", &brandIndex, &firstTypeSum, &secondTypeSum, &thirdTypeSum, &fourthTypeSum);
-
-                if (brandIndex < 0 || brandIndex > NUM_OF_TYPES) {
-                    printf("This brand is not valid\n");
-                    break;
-                }
-
-                populateForAllBrands(brandIndex, firstTypeSum, secondTypeSum, thirdTypeSum, fourthTypeSum);
-
-                while (processedCount < NUM_OF_BRANDS) {
-                    scanf(" %d %d %d %d %d", &brandIndex, &firstTypeSum, &secondTypeSum, &thirdTypeSum, &fourthTypeSum);
-                    populateForAllBrands(brandIndex, firstTypeSum, secondTypeSum, thirdTypeSum, fourthTypeSum);
-                }
-
-                break;
-
-            case stats:
-                int day = 0;
-                printf("What day would you like to analyze?");
-                scanf(" %d", &day);
-
-                while (cube[day][brandIndex][0] == -1 || day < 0 || day > DAYS_IN_YEAR) {
-                    printf("Please enter a valid day\nWhat day would you like to analyze?\n");
-                    scanf(" %d", &day);
-                }
-
-
-            default:
-                printf("Invalid input\n");
-        }
-        printMenu();
-        scanf("%d", &choice);
-    }
-    printf("Goodbye!\n");
-    return 0;
+void printTypeByIndex(int index) {
+    printf("%s", types[index]);
 }
